82e983197d6965595daaf66aaec1c0b07a467c78
Kevin <94081000+kmcclaning@users.noreply.github.com>
2022-08-04 18:17:33 -0400

    Update test_engNotation.py
    Updated license

--- test_engNotation.py
+++ test_engNotation.py
@@ -1,11 +1,33 @@
 """
 Test engNotation.py engineering notation number pretty-printer
 """
-__author__ = "KJ McClaning"
+
+__author__ = "K.J. McClaning"
 __created__ = "2022-07-26"
-__updated__ = "2022-07-26"
-__version__ = "0.0.1"
-__copyright__ = ""
+__updated__ = "2022-08-04"
+__version__ = "0.0.2"
+
+#  MIT License
+#
+#  Copyright (c) 2022 K.J.McClaning
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
 
 import math
 from unittest import TestCase
@@ -299,8 +321,8 @@ class TestEngNotation(TestCase):
 
     def test_eng_notation(self):
         """
-        test the routines to combine redundant elements
-        :returnn boolean: assertion results
+        test the engineering notation routines
+        :return boolean: assertion results
         """
         testCount = 1
         for thisTestD in testL:
